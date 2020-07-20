d41a7514858a10c46e352b20fc2e324505a70d16
Manuel Carrillo <manucaralmo@gmail.com>
2020-07-20 16:51:04 +0200

    variable protegida

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://127.0.0.1:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://manucaralmo:wtmRe45tFhvZlNtP@cluster0.hn1yn.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
